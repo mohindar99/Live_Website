8bae55865c417e5f9d755333493b0828927622b8
Mohindar Amarnadh <d.mohindar1999@gmail.com>
2022-10-22 06:41:57 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       <Route path='/about' element={<About/>} />
       <Route path='/contact' element={<Contact/>} />
       <Route path='/services' element={<Services />} />
-      <Route path='*' element={<Error />} />
+      <Route path='*' element={<Home />} />
     </Routes>
     <Footer/>
     </>
